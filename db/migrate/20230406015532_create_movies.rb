b84e0ffe6157d8f8c3bdcbc42343081985d89e18
João Carlos <joaocarlopa@gmail.com>
2023-04-06 00:27:10 -0300

    Feature: created movies table

--- db/migrate/20230406015532_create_movies.rb
+++ db/migrate/20230406015532_create_movies.rb
@@ -2,11 +2,16 @@ class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
       t.string :title
-      t.string :genre
-      t.string :year
+      t.string :gender
+      t.string :release_year
       t.string :country
-      t.date :published_at
-      t.text :description
+      t.string :director
+      t.string :duration
+      t.string :rating
+      t.date   :date_added
+      t.text   :description
+      t.text   :listed_in
+      t.text   :cast
 
       t.timestamps
     end
