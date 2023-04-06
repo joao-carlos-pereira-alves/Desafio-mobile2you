cdfd10425c54ae3d7df0bfcddbec28a18f9bbf62
João Carlos <joaocarlopa@gmail.com>
2023-04-05 22:32:11 -0300

    Feature: setup completed

--- spec/factories/user_factory.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :user do
-    # name { Faker::TvShows::TheOffice.character }
-    password { '123456789' }
-    email { Faker::Internet.unique.email }
-  end
-end
