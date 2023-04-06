1968662d0afc6756f342a8202042d8a928a9b5b5
João Carlos <joaocarlopa@gmail.com>
2023-04-06 00:25:19 -0300

    Feature: created movies controller and created "import" method for extract csv data

---
+++ app/controllers/api/v1/movies_controller.rb
@@ -0,0 +1,79 @@
+require 'csv'
+
+module Api
+  module V1
+    # Movies Controller
+    class MoviesController < ApplicationController
+      before_action :set_movie, only: %i[ show update destroy ]
+
+      # GET /movies
+      # GET /movies.json
+      def index
+        @movies = Movie.all
+      end
+
+      # GET /movies/1
+      # GET /movies/1.json
+      def show
+      end
+
+      # POST /movies
+      # POST /movies.json
+      def create
+        @movie = Movie.new(movie_params)
+
+        if @movie.save
+          render :show, status: :created
+        else
+          render json: @movie.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /movies/1
+      # PATCH/PUT /movies/1.json
+      def update
+        if @movie.update(movie_params)
+          render :show, status: :ok
+        else
+          render json: @movie.errors, status: :unprocessable_entity
+        end
+      end
+
+      def import
+        file_path =        Rails.root.join('db', 'netflix_titles.csv')
+        file =             File.read(file_path)
+        permitted_params = Movie.new.attributes.keys
+
+        Movie.transaction do
+          CSV.parse(file, headers: true) do |row|
+            movie_hash = row.to_h.slice(*permitted_params)
+            movie = Movie.new(movie_hash)
+
+            movie.save! if movie.valid?
+          end
+        end
+
+        render json: { message: "#{Movie.count} movies imported successfully." }
+      end
+
+      # DELETE /movies/1
+      # DELETE /movies/1.json
+      def destroy
+        @movie.destroy
+      end
+
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_movie
+          @movie = Movie.find(params[:id])
+        end
+
+        # Only allow a list of trusted parameters through.
+        def movie_params
+          params.require(:movie).permit(:title, :gender, :release_year, :country, :director, 
+                                        :duration, :rating, :date_added, :description, 
+                                        :listed_in, :cast, :updated_at)
+        end
+    end
+  end
+end
